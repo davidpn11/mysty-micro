3e0d497cf2694d0c15390fd33157c3de0bb495d4
David Nascimento <davidpn.11@gmail.com>
2023-04-18 15:39:50 -0600

    first commit with libs

---
+++ packages/libs/tsconfig/@types/federation.d.ts
@@ -0,0 +1,14 @@
+type MountParams = {
+  initialPath: string;
+  onNavigate(location: unknown): void;
+  defaultHistory?: unknown;
+  optimizely?: unknown;
+};
+
+type MountFn = (
+  element: Element | null,
+  params?: MountParams
+) => {
+  onParentNavigate(location: unknown): void;
+  unmount(): void;
+};
